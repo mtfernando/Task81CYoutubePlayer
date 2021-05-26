73229970182acff169da1943e51c8a2079158051
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-05-27 01:17:00 +1000

    PlayActivity completed

--- app/src/main/java/com/example/task81cyoutubeplayer/PlayActivity.java
+++ app/src/main/java/com/example/task81cyoutubeplayer/PlayActivity.java
@@ -2,13 +2,43 @@ package com.example.task81cyoutubeplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
-public class PlayActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
+public class PlayActivity extends YouTubeBaseActivity {
+    public static final String TAG = "PlayActivity";
+    YouTubePlayerView youTubePlayerView;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+    String videoCode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        Intent intent = getIntent();
+        videoCode = intent.getStringExtra("videoCode");
+        System.out.println(videoCode);
+        youTubePlayerView = findViewById(R.id.playerView);
+
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo(videoCode);
+                System.out.println(videoCode);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d(TAG, " Initialization Failed!");
+            }
+        };
+
+        //Initializing player
+        youTubePlayerView.initialize(YoutubeConfig.getApiKey(), onInitializedListener);
     }
 }
