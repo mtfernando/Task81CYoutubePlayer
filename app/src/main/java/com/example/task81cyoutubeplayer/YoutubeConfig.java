f2805ff9da198dde9c344766264de97447430f0f
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-05-26 22:35:22 +1000

    YoutubeConfig Added

---
+++ app/src/main/java/com/example/task81cyoutubeplayer/YoutubeConfig.java
@@ -0,0 +1,12 @@
+package com.example.task81cyoutubeplayer;
+
+public class YoutubeConfig {
+    public YoutubeConfig() {
+    }
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+
+    public static final String API_KEY = "AIzaSyAuuYM85VQZh7Fs1Yw6mcwd2CmjFH8VVf4";
+}
